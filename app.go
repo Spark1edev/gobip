21034c690d662c91fd761d668b2a978f20142017
Spark1edev <alterraltd1914@gmail.com>
2023-07-11 01:17:04 +0300

    1

--- app.go
+++ app.go
@@ -4,9 +4,9 @@ import "github.com/gofiber/fiber/v2"
 
 func main() {
 	app := fiber.New()
-
+	app.Static("/", "./views/src")
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("ok")
+		return c.SendFile("./views/src/index.html")
 	})
 
 	err := app.Listen("45.12.239.218:8080")
