c603b5b314651ba793c1b67e7545b76d77de44e5
Spark1edev <alterraltd1914@gmail.com>
2023-07-11 01:06:50 +0300

    1

--- app.go
+++ app.go
@@ -1,5 +1,13 @@
-package gobip
+package main
+
+import "github.com/gofiber/fiber/v2"
 
 func main() {
+	app := fiber.New()
 
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("ok")
+	})
+
+	app.Listen("http://45.12.239.218:8080/")
 }
